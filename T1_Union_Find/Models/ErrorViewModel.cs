60b5e60f79219af5b7fd043252e5f710ad8a7488
julia-hrytsyk <julia.hrytsyk@gmail.com>
2018-11-28 22:50:17 +0100

    Just created .Net Core Web Application

---
+++ T1_Union_Find/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace T1_Union_Find.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
